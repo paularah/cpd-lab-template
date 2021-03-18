115d5bdfaeda823b9a2161c50045e982b60a2288
arah <p.arah@alustudent.com>
2021-03-18 18:03:32 +0200

    update path to match node js version

--- routes.py
+++ routes.py
@@ -4,6 +4,7 @@ from fastapi.responses import JSONResponse
 from s3 import s3_auth
 from upload import upload_file_to_s3
 from settings import settings
+from typing import Optional
 from schema import FileList
 from controller import get_all_files
 
@@ -12,7 +13,7 @@ router = APIRouter()
 @router.post("/upload", status_code=status.HTTP_201_CREATED, summary="Upload files to AWS S3 Buckets",
              description="Upload a file to AWS S3 bucket", name="UPLOAD file to AWS S3",
              response_description="uploaded file successfully")
-async def upload_file(folder: str, s3: BaseClient = Depends(s3_auth), file_obj: UploadFile = File(...)):
+async def upload_file(folder: Optional[str] = '/', s3: BaseClient = Depends(s3_auth), myFile: UploadFile = File(...)):
       """
        call upload_file_to_s3 function and pass boto3 client
      """  
